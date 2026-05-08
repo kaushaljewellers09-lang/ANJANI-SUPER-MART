672ec206470895cb634b849cf8ed472c905c6c57
kaushaljewellers09-lang <kaushaljewellers09@gmail.com>
2026-05-08 14:16:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function sendWhatsApp(itemName, price) {
-    let myNumber = "91XXXXXXXXXX"; // Apna 10 digit number yahan likhein
+    let myNumber = "918467820968"; // 
     let msg = "Namaste Anjani Mart, mujhe ye chahiye: " + itemName + " (Rs." + price + ")";
     let url = "https://wa.me/" + myNumber + "?text=" + encodeURIComponent(msg);
     window.open(url, "_blank");
