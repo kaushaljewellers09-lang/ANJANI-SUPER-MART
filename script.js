a9e380c4e14433384e82e970d6d8f8e297aabea0
kaushaljewellers09-lang <kaushaljewellers09@gmail.com>
2026-05-08 14:17:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function sendWhatsApp(itemName, price) {
     let myNumber = "918467820968"; // 
     let msg = "Namaste Anjani Mart, mujhe ye chahiye: " + itemName + " (Rs." + price + ")";
-    let url = "https://wa.me/" + myNumber + "?text=" + encodeURIComponent(msg);
+    let url = "https://wa.me/" + 8467820968 + "?text=" + encodeURIComponent(msg);
     window.open(url, "_blank");
 }
